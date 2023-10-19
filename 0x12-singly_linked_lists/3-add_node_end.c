fcf02ffec6deb8f5d1cd60e0c6bed7962a26bab5
afnanmohamed <hegazi279@gmail.com>
2023-10-19 12:33:51 +0300

    test

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,33 +3,33 @@
 #include "lists.h"
 
 /**
- * append_node - Appends a new node to the tail of a linked list
+ * add_node_end - Appends a new node to the tail of a linked list
  * @list_head: Double pointer to the list_t list
  * @input_str: String to be included in the new node
  *
  * Return: Address of the new node, or NULL if the operation failed
  */
 
-list_t *append_node(list_t **list_head, const char *input_str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	list_t *current_node = *list_head;
+	list_t *current_node = *head;
 	unsigned int str_length = 0;
 
-	for (; input_str[str_length]; str_length++)
+	for (; str[str_length]; str_length++)
 	;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 	return (NULL);
 
-	new_node->str = strdup(input_str);
+	new_node->str = strdup(str);
 	new_node->len = str_length;
 	new_node->next = NULL;
 
-if (*list_head == NULL)
+if (*head == NULL)
 {
-*list_head = new_node;
+*head = new_node;
 return (new_node);
 }
 
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,41 +1,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
- * append_node - Appends a new node to the tail of a linked list
- * @list_head: Double pointer to the list_t list
- * @input_str: String to be included in the new node
+ * free_list - a function that frees a list_t list.
+ * @head: pointer to the first node in the list
  *
- * Return: Address of the new node, or NULL if the operation failed
+ * Return: void
  */
-list_t *append_node(list_t **list_head, const char *input_str)
+void free_list(list_t *head)
 {
-list_t *new_node;
-list_t *current_node = *list_head;
-unsigned int str_length = 0;
+	list_t *tempss;
 
-	for (; input_str[str_length]; str_length++)
-	;
-
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-	return (NULL);
-
-	new_node->str = strdup(input_str);
-	new_node->len = str_length;
-	new_node->next = NULL;
-
-	if (*list_head == NULL)
-{
-	*list_head = new_node;
-	return (new_node);
-}
-
-	for (; current_node->next; current_node = current_node->next)
-	;
-
-	current_node->next = new_node;
-
-	return (new_node);
+	while (head != NULL)
+	{
+		tempss = head;
+		head = head->next;
+		free(tempss->str);
+		free(tempss);
+	}
 }
