71af24c76489837cd2a97c32c7b5102651733827
afnanmohamed <hegazi279@gmail.com>
2023-10-20 14:52:20 +0300

    task0

--- 0x10-variadic_functions/ 0-sum_them_all.c
+++ 0x10-variadic_functions/ 0-sum_them_all.c
@@ -1,39 +1,30 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdlib.h>
 
-/**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: The number of parameters passed to the function.
- * @...: A variable number of parameters to calculate the sum of.
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
- */
-int sum_them_all(const unsigned int n, ...)
-{
-    va_list ap;
-    int *numbers;
-    unsigned int i;
-    int sum = 0;
 
-    if (n == 0)
-        return 0;
+	/**
+	 * sum_them_all - Returns the sum of all its paramters.
+	 * @n: The number of paramters passed to the function.
+	 * @...: A variable number of paramters to calculate the sum of.
+	 *
+	 * Return: If n == 0 - 0.
+	 *         Otherwise - the sum of all parameters.
+	 */
+	int sum_them_all(const unsigned int n, ...)
+	{
+		va_list ap;
+		unsigned int i, sum = 0;
 
-    numbers = malloc(n * sizeof(int));
-    if (numbers == NULL)
-        return 0;
 
-    va_start(ap, n);
+		va_start(ap, n);
 
-    for (i = 0; i < n; i++)
-        numbers[i] = va_arg(ap, int);
 
-    va_end(ap);
+		for (i = 0; i < n; i++)
+			sum += va_arg(ap, int);
 
-    for (i = 0; i < n; i++)
-        sum += numbers[i];
 
-    free(numbers);
+		va_end(ap);
 
-    return sum;
-}
+
+		return (sum);
+	}
