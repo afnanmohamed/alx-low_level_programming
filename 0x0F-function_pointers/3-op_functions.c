5d192aec9ea4fafd933157522397e6b009fdfdb8
afnanmohamed <hegazi279@gmail.com>
2023-10-22 10:16:32 +0300

    kolp

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,68 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
+
 
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_add - Adds two integers together
+ * @a: The first integer to be added
+ * @b: The second integer to be added
  *
- * Return: The sum of a and b.
+ * This function takes two integers as parameters,
+ * adds them together, and returns the result.
+ *
+ * Return: The sum of the two integers
  */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
+	int op_add(int a, int b)
+	{
+		return (a + b);
+	}
 /**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_sub - short description
+ * @a: first parameter
+ * @b: second parameter
  *
- * Return: The difference of a and b.
+ * Longer description
+ * Return: what does the function return
  */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
+	int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
 /**
  * op_mul - Returns the product of two numbers.
  * @a: The first number.
  * @b: The second number.
- *
  * Return: The product of a and b.
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
+*/
+
+	int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+
 /**
  * op_div - Returns the division of two numbers.
  * @a: The first number.
  * @b: The second number.
- *
  * Return: The quotient of a and b.
  */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
+	int op_div(int a, int b)
+	{
+		return (a / b);
+	}
 /**
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
  * @b: The second number.
- *
  * Return: The remainder of the division of a by b.
  */
-int op_mod(int a, int b)
-{
-	return (a % b);
-}
+	int op_mod(int a, int b)
+	{
+		return (a % b);
+	}
